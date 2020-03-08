5ca1f1e15eeccebff5fbc0bd0caf0eb0b94d661b
Keturah Howard <keturah.dev@gmail.com>
2020-03-07 18:07:10 -0800

    adjust Calculate methods, creat AddToTotal method

--- Models/MenuItems.cs
+++ Models/MenuItems.cs
@@ -14,11 +14,11 @@ namespace Bakery.Models
       {
         if (i % 3 == 0)
         {
-          price = price +=0 ; //its free
+          price +=0 ; //its free
         }
         else
         {
-          price = price += 5;
+          price += 5;
         }
       }
       return price;
@@ -38,14 +38,19 @@ namespace Bakery.Models
     }
     public int CalculatePrice()
     {
-      if (Amount == 3)
+      int price = 0;
+      for (int i = 1; i <= Amount; i++)
       {
-        return 5;
+        if (i % 3 == 0 && i < 4)
+        {
+          price = 5 ;
+        }
+        else
+        {
+          price += 2;
+        }
       }
-      else
-      {
-        return Amount * 2;
-      }
+      return price;
     }
     public void AddToTotalAmount(int amount)
     {
